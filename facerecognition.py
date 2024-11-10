b391d7e0ca81c8bdc1cc5a033b28e42f1dad26b4
Loading <dhourda@gmail.com>
2024-11-10 16:02:27 +0100

    live view window

--- facerecognition.py
+++ facerecognition.py
@@ -1,4 +1,30 @@
-print("hello World")
+import cv2
+#Spustitelne pomoci prikazu ' python3 <misto souboru> '
+# Otevre prvni pripojenou kameru, prvni byva na indexu
+cap = cv2.VideoCapture(0)
 
-while True :
-    print("helloWorld")
+# Jestli se kamera neotevrela spravne
+if not cap.isOpened():
+    print("Nelze otevřít kameru")
+    exit()
+
+# Cyklus pro cteni a potom zobrazeni snimku
+while True:
+    # Cteni snimku z kamery
+    ret, frame = cap.read()
+    
+    # Pokud neco selze
+    if not ret:
+        print("Nelze přečíst snímek")
+        break
+
+    # Zobrazeni obrazu v okne s nazvem Camera Feed
+    cv2.imshow('Camera Feed', frame)
+    
+    # Po jedne ms a zmacknuti q se okno zavre
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+#Ukonceni procesu a zavreni okna
+cap.release()
+cv2.destroyAllWindows()
